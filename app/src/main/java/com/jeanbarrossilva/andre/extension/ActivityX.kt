e3a4188b0c239e6e850af02faf2efcb50a66c89f
Jean Silva <jeanbarrossilva@outlook.com>
2021-03-05 22:53:35 -0300

    Fix Send feeback option in settings

--- app/src/main/java/com/jeanbarrossilva/andre/extension/ActivityX.kt
+++ app/src/main/java/com/jeanbarrossilva/andre/extension/ActivityX.kt
@@ -1,16 +1,13 @@
 package com.jeanbarrossilva.andre.extension
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
-import android.net.Uri
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.children
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.jeanbarrossilva.andre.extension.MaterialDialogX.showSendFeedbackErrorDialog
 import com.jeanbarrossilva.andre.extension.ViewGroupX.find
 
 object ActivityX {
@@ -21,21 +18,21 @@ object ActivityX {
     val Activity.toolbar get() = find<Toolbar>()
     val Activity.view get() = content?.children?.first() as? ViewGroup
     
-    @SuppressLint("QueryPermissionsNeeded")
-    private fun Activity.canStart(intent: Intent) = intent.resolveActivity(packageManager) != null
-    
     private inline fun <reified V : View> Activity.find() = view?.find<V>()
     
     fun Activity.composeEmail(
         receiver: String = "",
         subject: String = "",
         body: String = ""
-    ) = Intent(Intent.ACTION_SENDTO)
-        .apply {
-            type = "text/plain"
-            data = Uri.parse(receiver)
-            putExtra(Intent.EXTRA_SUBJECT, subject)
-            putExtra(Intent.EXTRA_TEXT, body)
-        }
-        .let { if (canStart(it)) startActivity(it) else showSendFeedbackErrorDialog(this) }
+    ) {
+        val emailIntent =
+            Intent(Intent.ACTION_SEND).apply {
+                putExtra(Intent.EXTRA_EMAIL, receiver)
+                putExtra(Intent.EXTRA_SUBJECT, subject)
+                putExtra(Intent.EXTRA_TEXT, body)
+            }
+        val emailAppChooserIntent = Intent.createChooser(emailIntent, null)
+        
+        startActivity(emailAppChooserIntent)
+    }
 }
