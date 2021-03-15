d7da777a2f339064b67f274d66a93e85a215157b
Jean Silva <jeanbarrossilva@outlook.com>
2021-03-15 17:48:11 -0300

    Add color to Subarea

--- app/src/main/java/com/jeanbarrossilva/andre/core/Subarea.kt
+++ app/src/main/java/com/jeanbarrossilva/andre/core/Subarea.kt
@@ -1,6 +1,8 @@
 package com.jeanbarrossilva.andre.core
 
 import android.content.Context
+import android.graphics.Color
+import androidx.annotation.ColorInt
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import com.jeanbarrossilva.andre.R
@@ -8,52 +10,49 @@ import com.jeanbarrossilva.andre.R
 sealed class Subarea(
 	@DrawableRes val iconRes: Int,
 	val title: String,
+	@ColorInt val color: Int,
 	open var indicator: SubareaIndicator = SubareaIndicator.Unset
 ) {
-	constructor(context: Context, @DrawableRes iconRes: Int, @StringRes titleRes: Int):
-		this(iconRes, context.getString(titleRes))
+	constructor(
+		context: Context,
+		@DrawableRes iconRes: Int,
+		@ColorInt color: Int,
+		@StringRes titleRes: Int
+	): this(iconRes, context.getString(titleRes), color)
 
 	class Health(context: Context):
-		Subarea(context, R.drawable.ic_health_and_safety, R.string.Subarea_name_health)
+		Subarea(context, R.drawable.ic_health_and_safety, Color.parseColor("#450EE3"), R.string.Subarea_name_health)
 
 	class Lounge(context: Context):
-		Subarea(context, R.drawable.ic_golf_course, R.string.Subarea_name_lounge)
+		Subarea(context, R.drawable.ic_golf_course, Color.parseColor("#128FD2"), R.string.Subarea_name_lounge)
 
 	class Spirituality(context: Context):
-		Subarea(context, R.drawable.ic_hiking, R.string.Subarea_name_spirituality)
+		Subarea(context, R.drawable.ic_hiking, Color.parseColor("#E890B8"), R.string.Subarea_name_spirituality)
 
 	class Emotional(context: Context):
-		Subarea(context, R.drawable.ic_mood_bad, R.string.Subarea_name_emotional)
+		Subarea(context, R.drawable.ic_mood_bad, Color.parseColor("#F88484"), R.string.Subarea_name_emotional)
 
 	class Happiness(context: Context):
-		Subarea(context, R.drawable.ic_mood, R.string.Subarea_name_happiness)
+		Subarea(context, R.drawable.ic_mood, Color.parseColor("#675DCD"), R.string.Subarea_name_happiness)
 
 	class Intellect(context: Context):
-		Subarea(context, R.drawable.ic_psychology, R.string.Subarea_name_intellect)
+		Subarea(context, R.drawable.ic_psychology, Color.parseColor("#F48C45"), R.string.Subarea_name_intellect)
 
 	class Career(context: Context):
-		Subarea(context, R.drawable.ic_work, R.string.Subarea_name_career)
+		Subarea(context, R.drawable.ic_work, Color.parseColor("#0674B4"), R.string.Subarea_name_career)
 
 	class Contribution(context: Context):
-		Subarea(context, R.drawable.ic_thumb_up, R.string.Subarea_name_contribution)
+		Subarea(context, R.drawable.ic_thumb_up, Color.parseColor("#8A5A44"), R.string.Subarea_name_contribution)
 
 	class Finances(context: Context):
-		Subarea(context, R.drawable.ic_savings, R.string.Subarea_name_finances)
+		Subarea(context, R.drawable.ic_savings, Color.parseColor("#AACC00"), R.string.Subarea_name_finances)
 
 	class Family(context: Context):
-		Subarea(context, R.drawable.ic_family_restroom, R.string.Subarea_name_family)
+		Subarea(context, R.drawable.ic_family_restroom, Color.parseColor("#F08080"), R.string.Subarea_name_family)
 
 	class Love(context: Context):
-		Subarea(context, R.drawable.ic_favorite, R.string.Subarea_name_love)
+		Subarea(context, R.drawable.ic_favorite, Color.parseColor("#2F3E46"), R.string.Subarea_name_love)
 
 	class Social(context: Context):
-		Subarea(context, R.drawable.ic_groups, R.string.Subarea_name_social)
-
-	companion object {
-		/**
-		 * Returns a list of [Subarea]s that doesn't have an [SubareaIndicator.Unset] [indicator].
-		 **/
-		val List<Subarea>.indicative
-			get() = filter { subarea -> subarea.indicator !is SubareaIndicator.Unset }
-	}
+		Subarea(context, R.drawable.ic_groups, Color.parseColor("#FF9505"), R.string.Subarea_name_social)
 }
--- app/src/main/java/com/jeanbarrossilva/andre/viewmodel/SubareasViewModel.kt
+++ app/src/main/java/com/jeanbarrossilva/andre/viewmodel/SubareasViewModel.kt
@@ -1,13 +1,14 @@
 package com.jeanbarrossilva.andre.viewmodel
 
+import android.graphics.Color
 import android.util.Log
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import com.jeanbarrossilva.andre.BuildConfig
 import com.jeanbarrossilva.andre.core.SubareaIndicator
-import com.jeanbarrossilva.andre.extension.ColorX.randomColor
 import com.jeanbarrossilva.andre.fragment.SubareasFragment
 
 class SubareasViewModel(private val fragment: SubareasFragment): ViewModel() {
@@ -24,12 +25,15 @@ class SubareasViewModel(private val fragment: SubareasFragment): ViewModel() {
 	fun showSubareasInChart() {
 		val entries =
 			subareas.map { subarea ->
-				PieEntry(subarea.indicator.levelAsPercentage(), subarea.title)
+				PieEntry(
+					subarea.indicator.levelAsPercentage(),
+					subarea.title,
+					ContextCompat.getDrawable(fragment.requireContext(), subarea.iconRes)
+				).apply { icon?.setTint(Color.WHITE) }
 			}
 		val set =
 			PieDataSet(entries, "Subareas of ${area.title}").apply {
-				setDrawIcons(true)
-				colors = subareas.map { randomColor() }
+				colors = subareas.map { subarea -> subarea.color }
 			}
 		
 		with(fragment.binding.subareasChart) {
@@ -37,6 +41,7 @@ class SubareasViewModel(private val fragment: SubareasFragment): ViewModel() {
 			legend?.isEnabled = false
 			description?.isEnabled = false
 			isDrawHoleEnabled = false
+			setDrawEntryLabels(false)
 			invalidate()
 		}
 	}
