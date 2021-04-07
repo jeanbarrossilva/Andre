0eb77272362056e9e95384dcdbf3ef599b0f447b
Jean Silva <jeanbarrossilva@outlook.com>
2021-04-07 08:47:28 -0300

    Remove chart dependencies

--- app/src/main/java/com/jeanbarrossilva/andre/extension/ChartDataX.kt
+++
@@ -1,9 +0,0 @@
-package com.jeanbarrossilva.andre.extension
-
-import com.github.mikephil.charting.data.ChartData
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.interfaces.datasets.IDataSet
-
-object ChartDataX {
-	val <E: Entry, DS: IDataSet<E>> ChartData<DS>.dataSet: DS get() = dataSets.first()
-}
